5cc5a99c06b55bd6f107c88eb0d54ff1b08eea13
Ben <benk@nakednews.com>
2023-01-09 19:40:18 -0500

    DM-03: Configure Node-scheduler to run at scheduled times

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const nodemailer = require('nodemailer');
 const axios = require('axios');
 const schedule = require('node-schedule');
 
-function InitialRun() {
-	console.log('Running on Port 3000');
-}
+const WEEKDAY_RULE = new schedule.RecurrenceRule();
+WEEKDAY_RULE.dayOfWeek = [new schedule.Range(1, 5)];
+WEEKDAY_RULE.hour = 18;
+WEEKDAY_RULE.minute = 00;
 
-InitialRun();
+const WEEKEND_RULE = new schedule.RecurrenceRule();
+WEEKEND_RULE.dayOfWeek = [0];
+WEEKEND_RULE.hour = 11;
+WEEKEND_RULE.minute = 00;
+
+const WEEKDAY_JOB = schedule.scheduleJob(WEEKDAY_RULE, getDateOfCurrentEntry);
+const WEEKEND_JOB = schedule.scheduleJob(WEEKEND_RULE, getDateOfCurrentEntry);
+
+function getDateOfCurrentEntry() {
+	axios.get(process.env.WEBSITE).then(res => {
+		compareTime(res.data.title);
+	}).catch((err) => {
+		console.error(`Error fetching entry: ${err}`);
+	});
+}
